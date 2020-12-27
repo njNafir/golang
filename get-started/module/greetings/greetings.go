b870d43716fd1340d905cb55ec685f3ffe5a06c7
njNafir <mdanjenafir24434@gmail.com>
2020-12-27 12:35:01 +0600

    Update greetings.go file

--- get-started/module/greetings/greetings.go
+++ get-started/module/greetings/greetings.go
@@ -54,7 +54,8 @@ func Hello(name string) (string, error) {
 	}
 
 	// message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprint(randomFormat())
 	
 	return message, nil
 }
